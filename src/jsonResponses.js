9f5b15f67219822ae432996377841d452f15ac42
julshotal <jgh6364@g.rit.edu>
2019-09-26 20:07:23 -0400

    weird error about the urlStruct[request.method][parsedurl.pathname] thing

---
+++ src/jsonResponses.js
@@ -0,0 +1,78 @@
+const userInfo = {};
+
+const respond = (request, response, status, user) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(user));
+  response.end();
+};
+
+const respondEmpty = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.end();
+};
+
+const getUsers = (request, response) => {
+  const responseJSON = {
+    userInfo,
+  };
+
+  return respond(request, response, 200, responseJSON);
+};
+
+const headUsers = (request, response) => respondEmpty(request, response, 200);
+
+const notReal = (request, response) => {
+  const responseJSON = {
+    message: '404 Error',
+    id: 'notFound',
+  };
+
+  return respond(request, response, 404, responseJSON);
+};
+
+const headNotReal = (request, response) => respondEmpty(request, response, 404);
+
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Please enter the required information',
+  };
+
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParameters';
+    return respond(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  if (userInfo[body.name]) {
+    responseCode = 204;
+  } else {
+    userInfo[body.name] = {};
+  }
+
+  userInfo[body.name].name = body.name;
+  userInfo[body.name].age = body.age;
+
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respond(request, response, responseCode, responseJSON);
+  }
+
+  return respondEmpty(request, response, responseCode);
+};
+
+module.exports = {
+  getUsers,
+  notReal,
+  headUsers,
+  headNotReal,
+  addUser,
+};
---
+++ src/server.js
@@ -0,0 +1,64 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+
+const html = require('./htmlResponses.js');
+const json = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  GET: {
+    '/': html.getIndex,
+    '/style.css': html.getCSS,
+    '/getUsers': json.getUsers,
+    '/notReal': json.notReal,
+  },
+  HEAD: {
+    '/getUsers': json.headUsers,
+    '/notReal': json.headNotReal,
+  },
+  POST: {
+    '/addUser': handlePost,
+    '/notReal': json.notReal,
+  },
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    const res = response;
+    const body = [];
+
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    request.on('data', (chunk) => {
+      body.push(chunk); 
+    });
+
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+
+      json.addUser(request, res, bodyParams);
+    });
+  } else {
+    json.notReal(request, response);
+  }
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if(urlStruct[request.method][parsedUrl.pathname]){
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    urlStruct[request.method].notReal(request, response);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+console.log(`Listening on 127.0.0.1: ${port}`);
